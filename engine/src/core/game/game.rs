2dc0dbf6014f3a24e3c7b8bfd090254489f366b5
Camoraz <racamo2006@gmail.com>
2026-02-20 13:51:10 +0100

    slight refactor

--- engine/src/core/game/game.rs
+++ engine/src/core/game/game.rs
@@ -19,9 +19,79 @@ pub struct Game {
     queue: UnitQueue,
 
     turn_number: u32,
-    turns: Vec<Turn>,
-
+    history: Vec<Turn>,
     snapshots: Vec<GameSnapshot>,
+}
+
+impl Game {
+    pub fn new(
+        players: HashMap<PlayerId, Player>,
+        units: HashMap<UnitId, Unit>,
+        grid: Grid,
+        queue: UnitQueue,
+    ) -> Self {
+        Self {
+            players,
+            units,
+            grid,
+            queue,
+            turn_number: 0,
+            history: Vec::new(),
+            snapshots: Vec::new(),
+        }
+    }
+
+    /// Single mutation entry point
+    pub fn apply_proposed(
+        &mut self,
+        player_id: PlayerId,
+        action: ProposedAction,
+    ) -> Result<ResolvedAction, GameError> {
+        self.validate_turn(player_id)?;
+
+        let resolved = self.resolve_action(player_id, action)?;
+
+        self.record_action(resolved.clone());
+
+        self.maybe_snapshot();
+
+        Ok(resolved)
+    }
+}
+
+
+impl Game {
+    fn resolve_action(
+        &mut self,
+        player_id: PlayerId,
+        action: ProposedAction,
+    ) -> Result<ResolvedAction, GameError> {
+        match action {
+            ProposedAction::Move { path } => {
+                self.resolve_move(player_id, path)
+            }
+            ProposedAction::Attack { target, attack } => {
+                self.resolve_attack(player_id, target, attack)
+            }
+            ProposedAction::Spawn { unit, position } => {
+                unimplemented!();
+            }
+            ProposedAction::EndTurn => {
+                self.resolve_end_turn()
+            }
+            _ => unimplemented!()
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum GameError {
+    NotYourTurn,
+    InvalidUnit,
+    InvalidPath,
+    OutOfRange,
+    NotEnoughResources,
+    IllegalAction,
 }
 
 #[derive(Clone)]
@@ -35,7 +105,6 @@ struct GameSnapshot {
 
 struct Turn {
     turn_number: u32,
-    active_unit: UnitId,
     actions: Vec<ActionLog>,
 }
 
@@ -50,7 +119,6 @@ pub enum ResolvedAction {
     Move {
         unit_id: UnitId,
         path: Path,
-        final_position: Position,
     },
     Attack {
         attacker: UnitId,
@@ -68,7 +136,7 @@ pub enum ResolvedAction {
 }
 
 /// Clients propose actions through this protocol
-enum ProposedAction {
+pub enum ProposedAction {
     Move {
         path: Path,
     },
